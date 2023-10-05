0078b992a236755bd7f27f5be5a2ebad80683fe1
Victor Ciolac <victor.bcfraga@gmail.com>
2023-10-05 19:10:51 -0300

    Req. 4

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,16 +1,14 @@
 const { Router } = require('express');
 const crypto = require('crypto');
+const validateLogin = require('../middlewares/validateLogin');
 
 const loginRouter = Router();
 
 const HTTP_OK_STATUS = 200;
 
-loginRouter.post('/login', async (req, res) => {
+loginRouter.post('/login', validateLogin, async (req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   return res.status(HTTP_OK_STATUS).json({ token });
-  // const { email, password } = req.body;
-  // if (email === 'email@email.com' && password === '123456') {
-  // } 
 });
 
 module.exports = loginRouter;
