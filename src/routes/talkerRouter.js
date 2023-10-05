d4dcec45d926cabd73f2f3e66a4e63db3275c11c
Victor Ciolac <victor.bcfraga@gmail.com>
2023-10-05 17:53:34 -0300

    Req. 2

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -8,7 +8,17 @@ const HTTP_OK_STATUS = 200;
 talkerRouter.get('/talker', async (req, res) => {
   const data = await readAndWrite.readFile();
   const result = data.length > 0 ? data : [];
-  res.status(HTTP_OK_STATUS).json(result);
+  return res.status(HTTP_OK_STATUS).json(result);
+});
+
+talkerRouter.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await readAndWrite.readFile();
+  const result = data.find((talker) => talker.id === parseInt(id, 10));
+  if (!result) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(HTTP_OK_STATUS).json(result);
 });
 
 module.exports = talkerRouter;
